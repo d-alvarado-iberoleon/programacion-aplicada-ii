3624e2c92f63c4cf49a4a5581ab031cda6b4687e
d-alvarado-iberoleon <156126141+d-alvarado-iberoleon@users.noreply.github.com>
2024-01-09 13:12:03 -0600

    Clase 1 y 2 agregadas

---
+++ ejemplos/clase02_repaso-python/ejemplo1.py
@@ -0,0 +1,25 @@
+# Este programa describe la estructura de un programa en Python
+
+# Definición de función saludar
+def saludar():
+    print("¡Hola Mundo!")
+    
+# Programa principal
+a = 10
+b = 20.1
+c = a + b
+
+print(a)
+print(b+50)
+
+edad = 15
+if edad >= 18: # Los dos puntos indican que inicia un bloque de instrucciones 
+    print("Puede votar")
+else:
+    print("No puede votar")
+
+saludar() # Llamada a la función saludar, definida arriba
+
+
+
+
